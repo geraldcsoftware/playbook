13d94186256a5c223e4f3bc2cfabb5741648fdf6
Gerald Chifanzwa <gchifanzwa@gmail.com>
2026-04-08 12:09:54 +0200

    fix: print errors to stderr instead of silently exiting
    SilenceErrors suppresses Cobra's error output, but main.go was not
    printing the error either, causing failures to exit with code 1 and
    no visible message.

--- cmd/playbook/main.go
+++ cmd/playbook/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/geraldcsoftware/playbook/internal/cli"
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	if err := cli.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "\033[31m✗\033[0m  %s\n", err)
 		os.Exit(1)
 	}
 }
